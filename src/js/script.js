48fc2d4ca7ab7ae8c51633c0a0cb48722741af01
jadier <jadierb@nycstudents.net>
2026-01-13 01:51:28 +0000

    created the 5 javascribt objects

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+const colleges = [
+  {
+    rank: 1,
+    name: "new york city college of technology",
+    program: "google IT support certificate",
+    address: "300 Jay Street, Brooklyn, NY",
+    website: "https://www.citytech.cuny.edu/",
+    maps: "https://maps.app.goo.gl/F3SGRFV4zh1yvmcb8",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUufVAsr5OrhWvrcJBkbSFf18LdVVP30wWug&s"
+  },
+  {
+    rank: 2,
+    name: "guttman communty college",
+    program: "information technology",
+    address: "50 West 40th Street, New York,",
+    website: "https://guttman.cuny.edu/",
+    maps: "https://maps.app.goo.gl/W1upHP18Y2y4AsiNA",
+    image: "https://cunyhumanitiesalliance.commons.gc.cuny.edu/files/2021/01/Guttman_LOGO_Full_Horizontal_Color-1024x468.png"
+  },
+  {
+    rank: 3,
+    name: "la guardia communtiy college",
+    program: "computer information systems",
+    address: "1-10 Thomson Avenue, Long Island City, NY 11101",
+    website: "https://www.laguardia.edu/",
+    maps: "https://maps.app.goo.gl/D24onqGCVzZ47h2o9",
+    image: "https://keystoneacademic-res.cloudinary.com/image/upload/c_pad,w_640,h_304/dpr_auto/f_auto/q_auto/v1/element/15/159776_Laguardia.jpeg"
+  },
+  {
+    rank: 4,
+    name: "york collge",
+    program: "it services desk specialist",
+    address: "94-20 Guy R. Brewer Blvd, Jamaica, NY 11433",
+    website: "https://www.york.cuny.edu/",
+    maps: "https://maps.app.goo.gl/D24onqGCVzZ47h2o9",
+    image: "https://www.york.cuny.edu/@@site-logo/yc-logo_01r.png"
+  },
+  {
+    rank: 5,
+    name: "queensborough community college",
+    program: "internet and information technology",
+    address: "222-05 56th Avenue, Bayside, NY 11364",
+    website: "https://www.qcc.cuny.edu/",
+    maps: "https://maps.app.goo.gl/3yM8gk1zYFqprTtA9",
+    image: "https://www.tonygamino.com/images/portfolio/qcc-logo-old.jpg"
+  }
+];
